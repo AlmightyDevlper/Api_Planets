9ea7553ea2fec1bce7e2926e807a2574a2b06d85
Thomas <thomas_dvlper@hotmail.com>
2022-02-08 20:45:06 +0100

    Planets
    Clean project start up

---
+++ planets/planets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  planets
+//
+//  Created by mac on 08/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
