090ba1adcab7fb28b56e0a467bd5b4b20c6c80c3
Thomas <thomas_dvlper@hotmail.com>
2022-02-09 22:10:39 +0100

    setting homescreen layout
    Added background image
    Added searchView
    Added searchTextField (Will act as searchbar)
    Added searchIcon (Will act as a search button)

--- planets/planets/ViewController.swift
+++ planets/planets/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var searchView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.searchView.layer.cornerRadius = 20
+        self.searchView.layer.borderColor = CGColor.init(red: 253, green: 253, blue: 253, alpha: 1)
+        self.searchView.layer.borderWidth = 1
+        
     }
 
 
