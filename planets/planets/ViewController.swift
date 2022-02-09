e6addddf77444b9e5913c68f0bfd652e28675ad9
Thomas <thomas_dvlper@hotmail.com>
2022-02-09 23:21:50 +0100

    SearchButtonView
    Added a view special for the searchbutton.

--- planets/planets/ViewController.swift
+++ planets/planets/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var searchView: UIView!
+    @IBOutlet weak var searchButtonView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.searchButtonView.layer.cornerRadius = 10
+        self.searchButtonView.layer.borderColor = CGColor.init(red: 253, green: 253, blue: 253, alpha: 1)
+        self.searchButtonView.layer.borderWidth = 1
+        
         
         self.searchView.layer.cornerRadius = 20
         self.searchView.layer.borderColor = CGColor.init(red: 253, green: 253, blue: 253, alpha: 1)
